7c3be7ef280ce62a680357313f14b228c3bd5a35
Michael Liendo <mtliendo@gmail.com>
2021-10-19 11:23:28 -0500

    add project details

--- src/App.js
+++ src/App.js
@@ -7,53 +7,53 @@ const TOKEN = process.env.REACT_APP_LIGHT_TOKEN
 const LIGHT_ID = process.env.REACT_APP_TOKEN_ID
 console.log('the env vars', { TOKEN, LIGHT_ID })
 function App() {
-  const [bulbColor, setBulbColor] = useState('#FFD517')
-  const [color, setColor] = useState('#FFD517')
-  // the quick brown fox jumped over the lazy dog
-  const handleColorChange = (color) => {
-    setColor(color.hex)
-  }
-  // the quick brown jumped over the lazy dog
-  const handleButtonClick = async () => {
-    setBulbColor(color)
-    axios({
-      method: 'put',
-      url: `https://api.lifx.com/v1/lights/id:${LIGHT_ID}/state`,
-      headers: {
-        Authorization: `Bearer ${TOKEN}`,
-      },
-      data: {
-        color: color,
-      },
-    }).then((data) => {
-      console.log({ success: 'light changed!', data })
-    })
-  }
+	const [bulbColor, setBulbColor] = useState('#FFD517')
+	const [color, setColor] = useState('#FFD517')
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <MainContainer>
-          <LightBulb bulbColor={bulbColor} />
-          <SketchPicker color={color} onChange={handleColorChange} />
-        </MainContainer>
-        <button className="button" onClick={handleButtonClick}>
-          Change Light
-        </button>
-      </header>
-    </div>
-  )
+	const handleColorChange = (color) => {
+		setColor(color.hex)
+	}
+
+	const handleButtonClick = async () => {
+		setBulbColor(color)
+		axios({
+			method: 'put',
+			url: `https://api.lifx.com/v1/lights/id:${LIGHT_ID}/state`,
+			headers: {
+				Authorization: `Bearer ${TOKEN}`,
+			},
+			data: {
+				color: color,
+			},
+		}).then((data) => {
+			console.log({ success: 'light changed!', data })
+		})
+	}
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<MainContainer>
+					<LightBulb bulbColor={bulbColor} />
+					<SketchPicker color={color} onChange={handleColorChange} />
+				</MainContainer>
+				<button className="button" onClick={handleButtonClick}>
+					Change Light
+				</button>
+			</header>
+		</div>
+	)
 }
 const MainContainer = ({ children }) => (
-  <div
-    style={{
-      width: '100vw',
-      display: 'flex',
-      justifyContent: 'center',
-    }}
-  >
-    {children}
-  </div>
+	<div
+		style={{
+			width: '100vw',
+			display: 'flex',
+			justifyContent: 'center',
+		}}
+	>
+		{children}
+	</div>
 )
 
 export default App
